8866fc355992a9b8688ec959677020c3a165fa32
end-dot <aj.1803160@gmail.com>
2023-12-01 21:59:09 +0600

    Add two pointer problem 283

---
+++ TwoPointers/283_moveZeros.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) {
+                swap(nums[i],nums[k]);
+                k++;
+            }
+            
+        }
+    }
+};
