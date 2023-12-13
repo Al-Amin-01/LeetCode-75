033da94d8bddf530f60c63d43e79a6e5542bc25f
end-dot <aj.1803160@gmail.com>
2023-12-13 22:34:13 +0600

    Add BST problem 700

---
+++ BST/700_searchInBST.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* r=root;
+        while(r){
+            if(r->val==val) return r;
+            if(r->val<val) r=r->right;
+            else r=r->left;
+        }
+        return NULL;
+        
+    }
+};
