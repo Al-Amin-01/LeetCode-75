6739f0bb261296094c9a21121a80ab459437d83e
end-dot <aj.1803160@gmail.com>
2023-12-30 23:58:32 +0600

    Add dfs 236

---
+++ DFS/236_lowestCommonAncestor.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == p || root == q || root == NULL)
+            return root;
+        TreeNode *x = lowestCommonAncestor(root->left, p, q);
+        TreeNode *y = lowestCommonAncestor(root->right, p, q);
+        if (x == NULL)
+            return y;
+        if (y == NULL)
+            return x;
+        return root;
+    }
+};
